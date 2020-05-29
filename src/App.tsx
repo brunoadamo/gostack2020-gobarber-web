3c80cab95942c568e9d68b7912890c913c302ab4
Bruno Adamo <brunoaraujoadamo@gmail.com>
2020-05-29 18:41:47 -0300

    feature: starting with context auth

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,14 @@ import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import GlobalStyle from './styles/global';
 
+import AuthContext from './contex/AuthContex';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'Bruno' }}>
+      <SignIn />
+    </AuthContext.Provider>
+
     <GlobalStyle />
   </>
 );
