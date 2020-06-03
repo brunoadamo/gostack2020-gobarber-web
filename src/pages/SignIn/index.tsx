6fd53a4d3ce04f0143987055e0662f982cfff509
Bruno Adamo <brunoaraujoadamo@gmail.com>
2020-06-03 19:43:44 -0300

    feature: toast icons styles

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -54,7 +54,7 @@ const SignIn: React.FC = () => {
         }
 
         addToast({
-          type: 'error',
+          type: 'info',
           title: 'Erro na autenticação',
           description: 'Ocorreu um erro ao fazer login, cheque as credenciais',
         });
